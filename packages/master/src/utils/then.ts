2e74c26c2ab5d1f206c17c0527eb77b90db23d85
Tariod <mykhailovdmt@gmail.com>
2022-11-17 11:33:23 +0200

    refactor: use lambdas in then util operator

--- packages/master/src/utils/then.ts
+++ packages/master/src/utils/then.ts
@@ -1,15 +1,14 @@
 import { Observable } from 'rxjs';
 
 export function then<I, O>(value: O) {
-  return (observable: Observable<I>) => {
-    return new Observable<O>((subscriber) => {
-      return observable.subscribe({
+  return (source: Observable<I>) =>
+    new Observable<O>((subscriber) =>
+      source.subscribe({
         error: (err) => subscriber.error(err),
         complete: () => {
           subscriber.next(value);
           subscriber.complete();
         },
-      });
-    });
-  };
+      }),
+    );
 }
